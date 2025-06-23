36ba00adfcbc0315d8cd915054781672093f99f9
Davide Balestrino <balestrinodavide@gmail.com>
2025-06-23 15:08:44 +0200

    login fix

--- Prog/Server/server.js
+++ Prog/Server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const prodottiRoutes = require('./src/prodotti/prodotti_routes');
 const userRoutes = require('./src/utenti/utenti_routes');
 const sessionRoutes = require('./src/sessions/session_routes');
@@ -7,20 +8,16 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-
 app.use(express.json());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 30000, secure: false }
+}))
+
 app.use('/prodotti', prodottiRoutes);
 app.use('/utenti', userRoutes);
 app.use('/sessions', sessionRoutes);
 app.listen(port, () => console.log(`app listening on port ${port}!`));
 app.use(express.static(path.join(__dirname, '../public')));
-
-
-app.listen(3000, () => {
-  console.log('Server backend su http://localhost:3000');
-});
--- Prog/Server/src/utenti/utenti_controller.js
+++ Prog/Server/src/utenti/utenti_controller.js
@@ -65,6 +65,13 @@ const updateUser = (req, res) => {
     });
 }
 
+const getUsers = (req, res) => {
+    pool.query(queries.getUsers, (error, results) =>{
+        if(error) throw error;
+        res.status(200).json(results.rows);
+    });
+};
+
 
 
 module.exports = {
@@ -73,4 +80,5 @@ module.exports = {
     addUser,
     deleteUser,
     updateUser,
+    getUsers,
 };
